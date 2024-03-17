ea2263a7c2d36da5ee5945e8bcaea98be1d8fa01
IgnacioRojas <ignaciorojitotaka@gmail.com>
2024-03-17 18:46:32 -0300

    renamed to __init__.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import core
-
-def main():
-    core.sort_files(core.images_folder, core.image_extensions)
-    core.sort_files(core.documents_folder, core.document_extensions)
-
-if __name__ == "__main__":
-    main()
