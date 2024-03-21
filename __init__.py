87f09e320b5ad32fe07bf73e8014ac6f8f275aea
IgnacioRojas <ignaciorojitotaka@gmail.com>
2024-03-21 14:37:46 -0300

    serializes default config when the app starts

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
-from src.application import Application
+from core.application import Application
+
+from core.serialization import Serializer
 
 if __name__ == "__main__":
+    Serializer().create_default_config()
     app = Application()
     app.mainloop()
