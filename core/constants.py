0d64091786aa410e4cfe196bc5f24d15c6cee8d8
IgnacioRojas <ignaciorojitotaka@gmail.com>
2024-04-04 14:49:48 -0300

    documented code

--- core/constants.py
+++ core/constants.py
@@ -4,6 +4,9 @@ from enum import auto
 PROJECT_NAME = "FilesOrganizer"
 
 class DirectoryType(Enum):
+    """
+    This enum is used solely in the folder_content_giver.py
+    """
     PATH_OBJECT = auto()
     ABSOLUTE_PATH = auto()
     NAME_ONLY = auto()
