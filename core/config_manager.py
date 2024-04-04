1835165ccca2d57f14d9876e7b157720173120a6
IgnacioRojas <ignaciorojitotaka@gmail.com>
2024-04-04 14:44:54 -0300

    removed methods related to serialization

--- core/config_manager.py
+++ core/config_manager.py
@@ -7,93 +7,10 @@ from core.folder_content_giver import FolderContentGiver
 import core.constants as constants
 
 class ConfigManager():
-    # Folder where the user config will be stored 
-    DATA_DIRECTORY = pathlib.Path("data")
-    # Path of the file where the user config is stored
-    USER_CONFIG_PATH = pathlib.Path("userconfig.pickle")
-
     default_config_data = None
 
     # Constructor
     def __init__(self) -> None:
-        self._create_default_config()
-
-    @staticmethod
-    def print_workspaces(config):
-        workspaces :list= config["workspaces"]
-        print("\n\n\nWorkspaces")
-        for workspace in workspaces:
-            name = workspace["name"]
-            path = workspace["path"]
-            prefix = workspace["prefix"]
-            print(f"■\t{name} in {path}. Prefix = '{prefix}'")
-
-    def workspaces(self, config):
-        """
-        Returns the workspaces of the given config
-        """
-        workspaces = config["workspaces"]
-        return workspaces
-
-    def extensions(self, config):
-        """
-        Returns the extensions of the given config
-        """
-        extensions = config["extensions"]
-        return extensions
-    
-    @staticmethod
-    def workspace(config, path, prefix):
-        """
-        Creates a new workspace in the config obj
-        """
-        if not isinstance(path, pathlib.Path):
-            path = pathlib.Path(path)
-        workspaces = config["workspaces"]
-        workspaces.append({"name": path.name, "path": path, "prefix": prefix})
-        
-
-    @staticmethod
-    def extension(config, category, formats: list):
-        """
-        Creates a new extension in the config obj
-        """
-        extensions = config["extensions"]
-        extensions.append({"category": category, "formats": formats})
-
-    @staticmethod
-    def default_config():
-        return ConfigManager.default_config_data
-
-        
- 
-    #@NotUsed
-    def folder(self, from_path: pathlib.Path, prefix_name, last_name):
-        """
-        Creates a folder from a specified path, making its name in base of the prefix and the last name
-        """
-        path = from_path / f"{prefix_name} {last_name}"
-        path.mkdir(exist_ok=True)
-        
-    #@NotUsed
-    def move_files_to_folder(self):
-        raise NotImplementedError()
-
-    #@NotImplemented
-    def move_files_to_subfolder(self):
-        raise NotImplementedError()
-
-    def is_workspace_organized(self, config, workspace_name):
-        workspace = [ws for ws in config["workspaces"] if ws["name"] == workspace_name].pop()
-        workspace_path = workspace["path"]
-        workspace_files = FolderContentGiver(workspace_path).files()
-        return len(workspace_files) == 0
-        
-    def _user_config_exists(self):
-        return self._user_config_path().exists()
-    
- 
-    def _create_default_config(self):
         """
         Creates the default config data at the instantiation of the class object
         """
@@ -181,28 +98,31 @@ class ConfigManager():
             ".swift",  # Swift script
         ]
         document_formats = [
-                ".doc",   # Microsoft Word Document
-                ".docx",  # Microsoft Word Open XML Document
-                ".html",  # Hypertext Markup Language
-                ".htm",   # HTML (alternative extension)
-                ".odt",   # OpenDocument Text Document
-                ".pdf",   # Portable Document Format
-                ".xls",   # Microsoft Excel Spreadsheet
-                ".xlsx",  # Microsoft Excel Open XML Spreadsheet
-                ".ods",   # OpenDocument Spreadsheet
-                ".ppt",   # Microsoft PowerPoint Presentation
-                ".pptx",  # Microsoft PowerPoint Open XML Presentation
-                ".txt",   # Plain Text Document
-            ]        
-        compressed_formats = [".7z", ".zip", ".rar"]
-        custom_formats = []
+            ".doc",   # Microsoft Word Document
+            ".docx",  # Microsoft Word Open XML Document
+            ".html",  # Hypertext Markup Language
+            ".htm",   # HTML (alternative extension)
+            ".odt",   # OpenDocument Text Document
+            ".pdf",   # Portable Document Format
+            ".xls",   # Microsoft Excel Spreadsheet
+            ".xlsx",  # Microsoft Excel Open XML Spreadsheet
+            ".ods",   # OpenDocument Spreadsheet
+            ".ppt",   # Microsoft PowerPoint Presentation
+            ".pptx",  # Microsoft PowerPoint Open XML Presentation
+            ".txt",   # Plain Text Document
+        ]        
+        compressed_formats = [
+            ".7z",
+            ".zip", 
+            ".rar"
+        ]
+
 
         self.extension(self.default_config_data, "Images", image_formats)
         self.extension(self.default_config_data, "Videos", video_formats)
         self.extension(self.default_config_data, "Audios", audio_formats)
         self.extension(self.default_config_data, "Executables", executable_formats)
         self.extension(self.default_config_data, "Documents", document_formats)
-        self.extension(self.default_config_data, "Custom", custom_formats)
         self.extension(self.default_config_data, "Compressed", compressed_formats)
 
         # Add the workspaces
@@ -210,12 +130,58 @@ class ConfigManager():
         self.workspace(self.default_config_data, downloads_path, "Downloaded")
 
     @staticmethod
-    def _user_config_path():
+    def print_workspaces(config):
+        workspaces :list= config["workspaces"]
+        print("\n\n\nWorkspaces")
+        for workspace in workspaces:
+            name = workspace["name"]
+            path = workspace["path"]
+            prefix = workspace["prefix"]
+            print(f"■\t{name} in {path}. Prefix = '{prefix}'")
+
+    @staticmethod
+    def workspaces(config):
         """
-        Returns the Path to the user config file
+        Returns the workspaces of the given config
         """
-        return pathlib.Path(ConfigManager._project_path() / ConfigManager.DATA_DIRECTORY / ConfigManager.USER_CONFIG_PATH)
+        workspaces = config["workspaces"]
+        return workspaces
+
+    def extensions(self, config):
+        """
+        Returns the extensions of the given config
+        """
+        extensions = config["extensions"]
+        return extensions
     
+    @staticmethod
+    def workspace(config, path, prefix):
+        """
+        Creates a new workspace in the config obj
+        """
+        if not isinstance(path, pathlib.Path):
+            path = pathlib.Path(path)
+        workspaces = config["workspaces"]
+        workspaces.append({"name": path.name, "path": path, "prefix": prefix})
+        
+    @staticmethod
+    def extension(config, category, formats: list):
+        """
+        Creates a new extension in the config obj
+        """
+        extensions = config["extensions"]
+        extensions.append({"category": category, "formats": formats})
+
+    @staticmethod
+    def default_config():
+        return ConfigManager.default_config_data
+
+    def is_workspace_organized(self, config, workspace_name):
+        workspace = [ws for ws in config["workspaces"] if ws["name"] == workspace_name].pop()
+        workspace_path = workspace["path"]
+        workspace_files = FolderContentGiver(workspace_path).files()
+        return len(workspace_files) == 0
+
     @staticmethod
     def _project_path() -> pathlib.Path:
         """
