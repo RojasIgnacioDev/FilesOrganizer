647502dbb530ee0e558543eb1b145aec38f2a8d8
IgnacioRojas <ignaciorojitotaka@gmail.com>
2024-04-09 13:08:16 -0300

    added SpecialFolder class

--- core/filesystem.py
+++ core/filesystem.py
@@ -1,93 +1,70 @@
+import constants
+import os
+import pathlib
 import sys
 sys.path.append("F:/DATOS/Documents/Projects/Python Projects/FilesOrganizer")
-import pathlib
-import os
-import constants
 
-# class Workspace:
-
-    
-#     workspace_folders = {}
-
-#     def __init__(self):
-#         pass
-
-#     def create_folder(self, name="Unnamed", extension_list=[]):
-#         """
-#         Creates a folder that will only store files that have their extension included in the extension format list passed as argument
-#         """
-#         new_folder = self.Folder(name, extension_list)
-#         self.workspace_folders[name] = new_folder
-
-#     def get_folder(self, key):
-#         return self.workspace_folders[key]
-
-#     def _add_folder_to_workspace(self, folder_object):
-#         """
-        
-#         """
-#         raise NotImplementedError()
-
-
-
-class Configuration:
-    workspace_folders = {}
-    
-    def __init__(self):
-        self._new_folder("Images", constants.image_formats)
-        self._new_folder("Videos", constants.video_formats)
-        self._new_folder("Audios", constants.audio_formats)
-        self._new_folder("Executables", constants.executable_formats)
-        self._new_folder("Documents", constants.document_formats)
-        self._new_folder("Compressed", constants.compressed_formats)
-
-    def _new_folder(self, category_name, extension_list):
-        """
-        Adds a folder that will store files including one of the extensions passed as a list
-        """
-        self.workspace_folders[category_name] = Folder(category_name, extension_list)
-    
-# TODO: finish the folder class so it can store all the files stored in int
+# TODO: finish the folder class so it can store all the files stored in it
 # TODO: make a method that returns all its files stored in
 # TODO: make the folder moves its files that do not has one of the extensions to the parent dir
-class Folder:
-    name = None
-    path : pathlib.Path
-    extensions: list
-    files = []
 
-    def __init__(self, name: str, extensions: list):
-        self.name = name
-        self.path = pathlib.Path(constants.DOWNLOADS_PATH / name)
+class Workspace:
+    def __init__(self) -> None:
+        raise NotImplementedError()
+
+class Folder:
+    """Represents a directory as a folder"""
+    path: pathlib.Path
+    name = None
+
+    def __init__(self, path, name: str):
+        # self.path = pathlib.Path(constants.DOWNLOADS_PATH / name)
+        self.path = path / name
         self.path.mkdir(exist_ok=True)
+        self.name = self.path.name
+
+    def files(self) -> list:
+        """Returns a list of files"""
+        files_object_list = [
+            File(file_path) for file_path in self.path.iterdir() if file_path.is_file()]
+        return files_object_list
+
+class SpecialFolder(Folder):
+    """A type of folder that only holds files with specific extensions"""
+
+    extensions: list
+
+    def __init__(self, path, name: str, extensions: list):
+        super().__init__(path, name)
         self.extensions = extensions
-        self.files = [path for path in self.path.iterdir() if path.is_file()]
         self.remove_stranger_files()
 
     def remove_stranger_files(self):
-        """
-        Removes files that do not fulfill having one of the extensions specified in the instantiation of the class
-        """
-        
-        all_files = [dir for dir in self.path.iterdir() if dir.is_file()]
-        odd_files = [filename for filename in all_files if self._is_file_stranger(filename)]
+        """Returns a list of files that do not fulfill having one of the extensions specified in the instantiation of the class"""
+        all_files = self.files()
+        odd_files = [
+            file for file in all_files if self._is_file_stranger(file)]
+        # TODO: finish the functionality of the method
 
-    def _is_file_stranger(self, filename: pathlib.Path):
+    def _is_file_stranger(self, file):
+        file_extension = file.extension
         for extension in self.extensions:
-            file_extension = os.path.splitext(filename)
-            pass
-        return True
+
+            if not file_extension == extension:
+                return True
+
+        return False
 
 class File:
+    """Represents a file"""
+
     def __init__(self, path: pathlib.Path):
         assert path.is_file(), "Expected a file path"
         self.path = path
         self.name = self.path.name
         self.root = self.path.stem
         self.extension = self.path.suffix
-        
-###
+
 # For debugging
-###
-Configuration()
+Folder(constants.DOWNLOADS_PATH, "Audios")
 pass
